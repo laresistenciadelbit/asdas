6d54c2a088239211cd61fbedcef7a8f235b287c1
noel <noelperal@yahoo.es>
2021-02-01 00:09:16 +0100

    [a] more db abstraction

--- model/sqlite.php
+++ model/sqlite.php
@@ -22,20 +22,10 @@ class Mydb extends SQLite3
 			}
 		}while(!$ret);
 	}
-
-	function load_config()
-	{
-		$ret = $this->query("SELECT * FROM config;");
-		if(!$ret && DEBUG) {
-			echo $this->lastErrorMsg();
-		}
-		while($row = $ret->fetchArray(SQLITE3_ASSOC) ){$config=$row;}
-		return $config;
-	}
 	
-	function insert_station_data($station,$sensor_name,$time,$sensor_value)
+	function insert($query)
 	{
-		$ret = $this->exec("INSERT INTO station_sensors values ('".$station."','".$sensor_name."','".$time."','".$sensor_value."');");
+		$ret = $this->exec($query);
 		if(!$ret) {
 			if(DEBUG)
 				return $this->lastErrorMsg();
@@ -46,69 +36,26 @@ class Mydb extends SQLite3
 			return CORRECT_RETURN;
 	}
 	
-	function insert_station_aditional_data($station,$status_name,$time,$status_value)
+	function update($query)
 	{
-		$ret = $this->exec("INSERT INTO station_status values ('".$station."','".$status_name."','".$time."','".$status_value."');");
-		if(!$ret) {
-			if(DEBUG)
-				return $this->lastErrorMsg();
-			else
-				return FAIL_RETURN;
-		}
-		else
-			return CORRECT_RETURN;
-	}
-	
-	function get_all_data($date,$station=NULL)
-	{
-		if(!is_null($date))
-		{
-			if( substr_count( $date,'-' )!=1 )
-				$where_date=" WHERE strftime('%Y-%m-%d',time) = '".$date."' "; //$where_date=" WHERE time = DATE('".$date."') ";
-			else
-			{
-				//$month_start=$date."/01";
-				//$month_end=$date."/31"; //irrelevante si nos pasamos de días totales de ese mes
-				//$where_date=" WHERE time between DATE('".$month_start."') AND DATE('".$month_end."') ";
-				$where_date=" WHERE strftime('%Y-%m',time) = '".$date."' ";
-			}
-			$where_station_prefix=" AND ";
-		}
-		else
-		{
-			$where_date="";
-			$where_station_prefix=" WHERE ";
-		}
-		
-		if(!is_null($station))
-			$where_station = $where_station_prefix." station='".$station."' ";
-		else
-			$where_station = "";		
-		//  *sqlite no permite full outer join (lo necesitamos ya que no todas las estaciones tienen que transmitir su estado o su gps)
-		//  *lo podemos emular con https://www.sqlitetutorial.net/sqlite-full-outer-join/
-		$ret = $this->query("SELECT se.station,se.sensor_name,se.time, se.sensor_value, st.status_name, st.status_value from station_sensors se 
-							LEFT JOIN station_status st USING(station,time) 
-							".$where_date." ".$where_station."
-							UNION ALL SELECT se.station,se.sensor_name,se.time, se.sensor_value, st.status_name, st.status_value from station_sensors se 
-							LEFT JOIN station_status st USING(station,time) 
-							WHERE station is null and time is null;");
-		if(!$ret && DEBUG) {
+		$ret = $this->exec($query);
+		if(!$ret && DEBUG ) {
 			echo $this->lastErrorMsg();
 		}
-		
-		// https://stackoverflow.com/questions/20694317/json-encode-function-special-characters
-		$array=array();
-		while($row = $ret->fetchArray(SQLITE3_ASSOC))
-		{
-			$row = array_map('utf8_encode', $row);
-			array_push($array,$row);
-		}
-		return json_encode($array);
+		if($ret)
+			return true;
+		else
+			return false;
 	}
-
-	function get_station_names()
+	
+	function select_simple($query) //devuelve un valor
 	{
-		$ret = $this->query("SELECT station from station_sensors group by station;");
+		return $this->querySingle($query);
+	}
+	
+	function select_string_array($query) //devuelve el array de strings, nada más.
+	{
+		$ret = $this->query($query);
 		if(!$ret && DEBUG) {
 			echo $this->lastErrorMsg();
 		}
@@ -117,31 +64,34 @@ class Mydb extends SQLite3
 		while($row = $ret->fetchArray(SQLITE3_ASSOC))
 		{	
 			$row=array_map('utf8_encode', $row);
-			array_push($array,$row['station']);
+			array_push($array,reset($row));	//https://www.designcise.com/web/tutorial/how-to-get-the-first-element-of-an-array-in-php
 		}
 		return $array;
 	}
-
-	function set_config($pass,$fm,$online_threshold_minutes,$primary_sensor,$primary_status)
+	
+	function select_row_array($query) //devuelve un array de una fila con el nombre de la columna como índice
 	{
-	
-		/*	// <- si quieremos que al escribir la configuración se guarde todo menos la contraseña si está vacía
-		$sql_query="UPDATE config set ";
-		if($pass!='')
-			$sql_query=$sql_query."pass='".$pass."' ,";
-		$sql_query=$sql_query." fm='".$fm."', online_threshold_minutes='".$online_threshold_minutes."', primary_sensor='".$primary_sensor."', primary_status='".$primary_status."';");
-		
-		$ret = $this->exec($sql_query);
-		*/
-	
-		$ret = $this->exec("UPDATE config set pass='".$pass."' , fm='".$fm."', online_threshold_minutes='".$online_threshold_minutes."', primary_sensor='".$primary_sensor."', primary_status='".$primary_status."';");
-		if(!$ret && DEBUG ) {
+		$ret = $this->query($query);
+		if(!$ret && DEBUG) {
 			echo $this->lastErrorMsg();
 		}
-		if($ret)
-			return true;
-		else
-			return false;
+		while($row = $ret->fetchArray(SQLITE3_ASSOC) ){$arr=$row;}
+		return $arr;	
+	}
+	
+	function select_json_array($query) //devuelve un array en formato json de todo el contenido
+	{
+		$ret = $this->query($query);
+		if(!$ret && DEBUG) {
+			echo $this->lastErrorMsg();
+		}
+		$array=array();
+		while($row = $ret->fetchArray(SQLITE3_ASSOC))
+		{
+			$row = array_map('utf8_encode', $row); // https://stackoverflow.com/questions/20694317/json-encode-function-special-characters
+			array_push($array,$row);
+		}
+		return json_encode($array);
 	}
 }
 ?>
